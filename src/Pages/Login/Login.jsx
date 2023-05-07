64e1b1ac4225144a7a62b0dc3235f3e2cba8664b
Shoaib Jamil <shoaibjamil43@gmail.com>
2023-05-07 17:14:48 +0500

    Making loginScreen

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,29 +1,61 @@
-import React from 'react'
-import { Typography, Button, Stack, Box } from "@mui/material";
+import React from "react";
+import "./Login.css";
+import { Typography, Button, Box, Grid, TextField, CssBaseline, Container } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export function Login() {
-    return (
-      <>
-      <Stack direction="row" justifyContent="center" sx={{height:"100vh"}}>
-        <Box  sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems:"center",
-              textAlign:"center"
-            }}>
-          <Typography variant="h1" sx={{marginBottom:"5px"}}>Welcome to the Azra Care Dashboard</Typography>
-          <Typography variant="body1">
-          Bringing order to the post-operative chaos - Log in here
+  return (
+    <>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box className="loginBox" >
+          <Typography component="h1" variant="h5">
+            Welcome To ToDoGo!
           </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
             <Button
-            sx={{ marginTop: "1rem",background:"#816179",width:"25%" }}
-            variant="contained"
-          >
-            Login
-          </Button>
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
         </Box>
-        </Stack>
-      </>
-    );
+      </Container>
+    </>
+  );
 }
