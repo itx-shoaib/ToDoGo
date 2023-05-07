d9a06704a3fb16dfadf05403c399d61cab954ff7
Shoaib Jamil <shoaibjamil43@gmail.com>
2023-05-07 16:25:36 +0500

    Making theme for material ui

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Typography, Button, Stack, Box } from "@mui/material";
 
 export function Login() {
-  return (
-    <div>login</div>
-  )
+    return (
+      <>
+      <Stack direction="row" justifyContent="center" sx={{height:"100vh"}}>
+        <Box  sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems:"center",
+              textAlign:"center"
+            }}>
+          <Typography variant="h1" sx={{marginBottom:"5px"}}>Welcome to the Azra Care Dashboard</Typography>
+          <Typography variant="body1">
+          Bringing order to the post-operative chaos - Log in here
+          </Typography>
+            <Button
+            sx={{ marginTop: "1rem",background:"#816179",width:"25%" }}
+            variant="contained"
+          >
+            Login
+          </Button>
+        </Box>
+        </Stack>
+      </>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,53 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  customeScondary: "#979797",
+  palette: {
+    primary: {
+      main: "#3f51b5",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    text: {
+      main: "#979797",
+      secondary: "#979797",
+    },
+  },
+  typography: {
+    allVariants: {
+      fontFamily: ["Lato", "Roboto", "-apple-system"].join(","),
+      color: "#343A40",
+      fontWeight: {
+        primary: {
+          main: 400,
+        },
+        secondary: {
+          main: 600,
+        },
+      },
+    },
+    h1: {
+      fontSize: 40,
+    },
+    h2: {
+      fontSize: 30,
+    },
+    body1: {
+      fontFamily: "Lato",
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
+  </ThemeProvider>
   </React.StrictMode>
 );
 
